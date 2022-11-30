1b99b9d97e8aa189d9ca2bfc73d20f5f3027ca9b
TianheWu <tianhe_wu@foxmail.com>
2022-11-30 23:39:19 +0800

    update baseline

--- data/wsi.py
+++ data/wsi.py
@@ -3,23 +3,20 @@ import os
 import cv2
 import numpy as np
 
-class WSI(torch.utils.data.Dataset):
-    def __init__(self, label_file_path=None, dataset_path=None):
-        super(WSI, self).__init__()
+
+class WSIData(torch.utils.data.Dataset):
+    def __init__(self, dataset_path=None):
+        super(WSIData, self).__init__()
         self.dataset_path = dataset_path
         self.dataset_name, self.class_label_list = [], []
 
-        file_name_list = []
         filenames = os.listdir(dataset_path)
         for file in filenames:
-            file_name_list.append(file)
-
-        with open(label_file_path, 'r') as listFile:
-            for line in listFile:
-                name, class_label = line[:-1].split(":")
-                if name in file_name_list:
-                    self.dataset_name.append(name)
-                    self.class_label_list.append(class_label)
+            self.dataset_name.append(file)
+            self.class_label_list.append(float(file[3]))
+        
+        self.class_label_list = np.array(self.class_label_list)
+        self.class_label_list = self.class_label_list.astype('float').reshape(-1, 1)
     
     def img_init_process(self, x):
         x = cv2.cvtColor(x, cv2.COLOR_BGR2RGB)
@@ -36,5 +33,5 @@ class WSI(torch.utils.data.Dataset):
         image = self.img_init_process(image)
         image = torch.from_numpy(image).type(torch.FloatTensor)
         class_label = self.class_label_list[idx]
-        class_label = torch.from_numpy(class_label).type(torch.FloatTensor)
+        class_label = torch.from_numpy(class_label).type(torch.LongTensor)
         return image, class_label
--- train.py
+++ train.py
@@ -1,19 +1,21 @@
+import os
 import torch
 import argparse
 import torch
 import torch.nn as nn
 import numpy as np
-import os
 import random
 import logging
-import tqdm
 import time
+import torch.distributed as dist
+import torch.multiprocessing as mp
 
-from data.wsi import WSI
+from data.wsi import WSIData
 from model.gcn import GCN
 from torch.utils.data import DataLoader
+from tqdm import tqdm
+from torch.nn.parallel import DistributedDataParallel as DDP
 
-os.environ['CUDA_VISIBLE_DEVICES'] = '0, 1, 2, 3'
 
 
 def setup_seed(seed):
@@ -27,90 +29,103 @@ def setup_seed(seed):
     torch.backends.cudnn.deterministic = True
 
 
-def set_logging(args):
-    if not os.path.exists("./output/log/"): 
-        os.makedirs("./output/log/")
-    filename = args.log_file_path
-    logging.basicConfig(
-        level=logging.INFO,
-        filename=filename,
-        filemode='w',
-        format='[%(asctime)s %(levelname)-8s] %(message)s',
-        datefmt='%Y%m%d %H:%M:%S'
-    )
+def set_log(args):
+    if not os.path.exists(args.log_file_path): 
+        os.makedirs(args.log_file_path)
+
+    filename = os.path.join(args.log_file_path, args.model_name + ".log")
+    log_format = logging.Formatter("%(asctime)s - %(levelname)s - %(name)s -   %(message)s")
+
+    writer = logging.getLogger()
+    fileHandler = logging.FileHandler(filename)
+    fileHandler.setFormatter(log_format)
+    writer.addHandler(fileHandler)
+    writer.setLevel(logging.INFO)
+    return writer
 
 
 def parse_args():
     parser = argparse.ArgumentParser(description='GCN')
 
     # dataset config
-    parser.add_argument('--log_file_path', type=str, default="./output/log/xxxx.txt")
-    parser.add_argument('--model_name', type=str, default="XXXX")
+    parser.add_argument('--log_file_path', type=str, default="./output/log/base_model")
+    parser.add_argument('--model_name', type=str, default="baseline-temp")
     parser.add_argument('--ckpt_path', type=str, default="./output/checkpoint/")
 
     # model train
-    parser.add_argument('--batch_size', type=int, default=8)
+    parser.add_argument('--batch_size', type=int, default=128)
     parser.add_argument('--num_workers', type=int, default=8)
-    parser.add_argument('--num_epochs', type=int, default=200)
+    parser.add_argument('--num_epochs', type=int, default=300)
     parser.add_argument('--lr', type=float, default=1e-5)
     parser.add_argument('--T_max', type=int, default=40)
     parser.add_argument('--eta_min', type=float, default=0.)
-    parser.add_argument('--weight_decay', type=float, default=1e-4)
+    parser.add_argument('--weight_decay', type=float, default=1e-6)
     parser.add_argument('--val_freq', type=int, default=1)
 
     # model config
-    parser.add_argument('--dim', type=int, default=64)
+    parser.add_argument('--input_dim', type=int, default=3)
+    parser.add_argument('--num_class', type=int, default=9)
+    parser.add_argument('--dim', type=int, default=32)
 
     # dataset
-    parser.add_argument('--train_label_file_path', type=str, default="./data/name_list.txt")
-    parser.add_argument('--valid_label_file_path', type=str, default="./data/name_list.txt")
-    parser.add_argument('--train_dataset_path', type=str, default="")
-    parser.add_argument('--valid_dataset_path', type=str, default="")
+    parser.add_argument('--train_dataset_path', type=str, default="/mnt/cpath2/lf/data/wsi_44_demo/train")
+    parser.add_argument('--valid_dataset_path', type=str, default="/mnt/cpath2/lf/data/wsi_44_demo/val")
     args = parser.parse_args()
     return args
 
 
-def run():
+def main(local_rank, args, train_dataset, valid_dataset):
+    args.local_rank = local_rank
+    writer = set_log(args)
+    args.ckpt_path = os.path.join(args.ckpt_path, args.model_name)
+
+    if not os.path.exists(args.ckpt_path): 
+        os.makedirs(args.ckpt_path)
+
+    dist.init_process_group("nccl", rank=args.local_rank, world_size=args.n_gpu)
+    torch.cuda.set_device(args.local_rank)
+
     setup_seed(20)
-    args = parse_args()
 
-    set_logging(args)
+    train_sampler = torch.utils.data.distributed.DistributedSampler(train_dataset)
 
-    TrainDataset = WSI(label_file_path=args.train_label_file_path, dataset_path=args.train_dataset_path)
-    ValidDataset = WSI(label_file_path=args.valid_label_file_path, dataset_path=args.valid_dataset_path)
-
-    train_loader = DataLoader(dataset=TrainDataset, batch_size=args.batch_size, 
-        num_workers=args.num_workers, drop_last=True, shuffle=True)
-    val_loader = DataLoader(dataset=ValidDataset, batch_size=args.batch_size,
+    train_loader = DataLoader(dataset=train_dataset, batch_size=args.batch_size, 
+        num_workers=args.num_workers, drop_last=True, shuffle=False, sampler=train_sampler)
+    val_loader = DataLoader(dataset=valid_dataset, batch_size=args.batch_size,
         num_workers=args.num_workers, drop_last=True, shuffle=False)
-    
-    net = GCN(dim=64)
-    net = nn.DataParallel(net).cuda()
-    logging.info('{} : {} [M]'.format('#Params', sum(map(lambda x: x.numel(), net.parameters())) / 10 ** 6))
 
-    # loss function
-    criterion = torch.nn.MSELoss()
+    net = GCN(num_class=args.num_class).cuda()
+    net = DDP(net, device_ids=[args.local_rank])
+    
+    if args.local_rank in [0, -1]:
+        writer.info('{} : {} [M]'.format('#Params', sum(map(lambda x: x.numel(), net.parameters())) / 10 ** 6))
+        writer.info('length train dataset {}'.format(len(train_dataset)))
+        writer.info('length val dataset {}'.format(len(valid_dataset)))
+
     optimizer = torch.optim.Adam(net.parameters(), lr=args.lr, weight_decay=args.weight_decay)
+    criterion = nn.CrossEntropyLoss()
     scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=args.T_max, eta_min=args.eta_min)
 
     best_acc = 0
-    for epoch in args.num_epochs:
+    for epoch in range(args.num_epochs):
         start_time = time.time()
-        logging.info('Running training epoch {}'.format(epoch + 1))
+
+        train_sampler.set_epoch(epoch)
+        if args.local_rank in [0, -1]:
+            writer.info('Running training epoch {}'.format(epoch + 1))
 
         net.train()
         loss_list = []
-        flag_list = []
+        correct_train = 0
 
-        for data in tqdm(train_loader):
+        for data in tqdm(train_loader, position=0):
             image, class_label = data
-            image = image.cuda()
-            class_label = class_label.cuda()
-            class_label = torch.squeeze(class_label).cuda()
+            image = image.cuda(non_blocking=True)
+            class_label = class_label.cuda(non_blocking=True)
+            class_label = torch.squeeze(class_label)
 
             pred_label = net(image)
-            pred_label = torch.squeeze(pred_label)
-
+            
             optimizer.zero_grad()
             loss = criterion(pred_label, class_label)
             loss_list.append(loss.item())
@@ -119,55 +134,57 @@ def run():
             optimizer.step()
             scheduler.step()
 
-            pred_label = pred_label.cpu().detach().numpy()
-            class_label = class_label.cpu().detach().numpy()
+            pred_label = pred_label.cpu()
+            class_label = class_label.cpu()
+            pred = pred_label.data.max(1, keepdim=True)[1]
+            correct_train += pred.eq(class_label.data.view_as(pred)).sum()
 
-            for i in range(pred_label.shape[0]):
-                flag = 1 if round(pred_label[i]) == class_label[i] else 0
-                flag_list.append(flag)
+        avg_loss = sum(loss_list) / len(loss_list)
+        acc_train = correct_train / (len(train_loader.dataset) / args.n_gpu)
 
-        avg_loss = loss_list / len(loss_list)
-        acc = sum(flag_list) / len(flag_list)
-        logging.info('--Training ACC: {:.2f}; Loss: {:.4f}'.format(acc, avg_loss))
+        if args.local_rank in [0, -1]:
+            writer.info('Training Loss: {:.8f}, Training Acc: {:.4f}'.format(avg_loss, acc_train))
+            writer.info('Starting eval...')
+            writer.info('Running val in epoch {}'.format(epoch + 1))
 
-        if (epoch + 1) % args.val_freq == 0:
-            logging.info('Starting eval...')
-            logging.info('Running val in epoch {}'.format(epoch + 1))
-            with torch.no_grad():
-                net.eval()
-                flag_list = []
-                for data in tqdm(val_loader):
-                    image, class_label = data
-                    image = image.cuda()
-                    class_label = class_label.cuda()
-                    class_label = torch.squeeze(class_label).cuda()
+        with torch.no_grad():
+            net.eval()
+            correct_val = 0
 
-                    pred_label = net(image)
-                    pred_label = torch.squeeze(pred_label)
+            for data in tqdm(val_loader):
+                image, class_label = data
+                image = image.cuda(non_blocking=True)
+                class_label = class_label.cuda(non_blocking=True)
+                class_label = torch.squeeze(class_label)
 
-                    pred_label = pred_label.cpu().detach().numpy()
-                    class_label = class_label.cpu().detach().numpy()
+                pred_label = net(image)
+                pred_label = pred_label.cpu()
+                class_label = class_label.cpu()
 
-                    for i in range(pred_label.shape[0]):
-                        flag = 1 if round(pred_label[i]) == class_label[i] else 0
-                        flag_list.append(flag)
+                pred = pred_label.data.max(1, keepdim=True)[1]
+                correct_val += pred.eq(class_label.data.view_as(pred)).sum()
+            
+            acc_val = correct_val / len(val_loader.dataset)
+            if acc_val > best_acc:
+                if not os.path.exists(args.ckpt_path):
+                    os.makedirs(args.ckpt_path)
+                best_acc = acc_val
+                if args.local_rank in [0, -1]:
+                    writer.info('best acc is ============================{:.4f}'.format(best_acc))
+
+                # save weights
+                model_name = "epoch{}.pt".format(epoch + 1)
+                model_save_path = os.path.join(args.ckpt_path, model_name)
                 
-                acc = sum(flag_list) / len(flag_list)
-                if acc > best_acc:
-                    if not os.path.exists(args.ckpt_path):
-                        os.makedirs(args.ckpt_path)
-                    best_acc = acc
-                    logging.info('======================================================================================')
-                    logging.info('======================================================================================')
-                    logging.info('best acc is {}'.format(best_acc))
-
-                    # save weights
-                    model_name = "epoch{}.pt".format(epoch + 1)
-                    model_save_path = os.path.join(args.ckpt_path, model_name)
-                    torch.save(net.module.state_dict(), model_save_path)
-                    logging.info('Saving weights and model of epoch{}, ACC:{}'.format(epoch + 1, best_acc))
-        
-        logging.info('Epoch {} done. Time: {:.2}min'.format(epoch + 1, (time.time() - start_time) / 60))
+                torch.save(net.module.state_dict(), model_save_path)
+                if args.local_rank in [0, -1]:
+                    writer.info('Saving weights and model of epoch{}, ACC:{:.4f}'.format(epoch + 1, best_acc))
+            
+            else:
+                if args.local_rank in [0, -1]:
+                    writer.info('Valid acc is {:.4f}'.format(acc_val))
+        if args.local_rank in [0, -1]:
+            writer.info('Epoch {} done. Time: {:.2} min'.format(epoch + 1, (time.time() - start_time) / 60))
 
 
 if __name__ == "__main__":
@@ -178,4 +195,13 @@ if __name__ == "__main__":
     os.environ['VECLIB_MAXIMUM_THREADS'] = str(cpu_num)
     os.environ['NUMEXPR_NUM_THREADS'] = str(cpu_num)
     torch.set_num_threads(cpu_num)
-    run()
+
+    args = parse_args()
+
+    os.environ["MASTER_ADDR"] = "127.0.0.1"
+    os.environ["MASTER_PORT"] = "18101"
+
+    args.n_gpu = torch.cuda.device_count()
+    train_dataset = WSIData(dataset_path=args.train_dataset_path)
+    valid_dataset = WSIData(dataset_path=args.valid_dataset_path)
+    mp.spawn(main, args=(args, train_dataset, valid_dataset), nprocs=args.n_gpu, join=True)
